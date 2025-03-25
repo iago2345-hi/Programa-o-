fad460c61b962cb80d4cc27509c3b537aa3e7d7d
iago2345-hi <iago.pereira23@etepd.com>
2025-03-25 09:58:42 -0300

    Add files via upload

---
+++ História I.py
@@ -0,0 +1,8 @@
+nome = input("Qual o seu nome de herói ?")
+idade = int(input("Qual a sua idade ?"))
+poder = input("Qual o seu poder ?")
+vilao = input("Qual o nome do vilão que você vai enfrentar ?")
+poder_vilao = input("Qual o poder do vilão ?")
+ajudante = input("Qual o nome da pessoa que vai te ajudar ?")
+
+print(f"Era uma vez uma história de um herói muito forte. O herói {nome} estava tranquilo em sua casa, com seus {idade} anos e aproveitadno sua vida de herói. Com seus poderes de {poder} ele derrotou inúmeros oponentes muito fortes. Mas, teve um vilão chamado {vilao} que quase destruiu a Terra que tinha um poder de {poder_vilao}. O herói {nome} com  a ajuda de um cidadão chamado {ajudante} conseguiram parar o vilão e salvar a Terra de um ser maligno. E todos foram felizes para sempre!")
