11b8cd10195f5316a16d6b13a94c8e76f3bf021d
iago2345-hi <iago.pereira23@etepd.com>
2025-04-01 16:31:52 -0300

    Update App.js

--- atvidadereact/App.js
+++ atvidadereact/App.js
@@ -1,36 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { TextInput } from 'react-native-web';
-import Touchable from './node_modules/react-native-web/src/exports/Touchable/index';
-import MemoedTouchableOpacity from './node_modules/react-native-web/dist/exports/TouchableOpacity/index';
-import Button from './node_modules/react-native-web/dist/exports/Button/index';
-import setAndForwardRef from './node_modules/react-native-web/dist/vendor/react-native/Utilities/setAndForwardRef';
-import AnimatedValueXY from './node_modules/react-native-web/dist/vendor/react-native/Animated/nodes/AnimatedValueXY';
-
+import React from 'react';
+import { StyleSheet, Text, View, Image, StatusBar } from 'react-native';
 export default function App() {
-  return (
-    <View style={styles.container}>     
-      <Text styles={styles.titulo}>Calcule seu IMC</Text>
-      <TextInput styles={styles.input} placeholder='Digite seu peso'></TextInput>
-      <TextInput styles={styles.input} placeholder='Digite sua altura'></TextInput>
-      <TouchableOpacity styles={styles.Button}>
-        <text styles={styles.Button}>calculadora</text>
-      </TouchableOpacity>
-      <status style="auto"/>
-    </View>
-  );
+return (
+<View style={styles.container}>
+<StatusBar barStyle="light-content" backgroundColor="#1f1f1f" />
+<View style={styles.card}>
+<Image
+source={{ uri: 'https://blog.estacio.br/wp-content/uploads/2022/07/o-que-filosofo-faz-780x450.jpeg' }}
+style={styles.profileImage}
+/>
+<Text style={styles.name}>Iago Leonam</Text>
+<Text style={styles.phrase}>“Formado em nada, estudante”</Text>
+</View>
+</View>
+);
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  titulo: {
-    fontsize: 50,
-    color:'#0ff',
-    margintop: 50,
-    textAlign: 'center'
-    padding: 20,
-    fontweight: 'bold',
-  }
+container: {
+flex: 1,
+backgroundColor: '#121212',
+alignItems: 'center',
+justifyContent: 'center',
+padding: 16,
+},
+card: {
+backgroundColor: '#1f1f1f',
+borderRadius: 20,
+padding: 24,
+alignItems: 'center',
+shadowColor: '#000',
+shadowOpacity: 0.3,
+shadowRadius: 10,
+elevation: 10,
+width: '100%',
+maxWidth: 320,
+},
+profileImage: {
+width: 120,
+height: 120,
+
+borderRadius: 60,
+marginBottom: 16,
+borderWidth: 2,
+borderColor: '#00d1ff',
+},
+name: {
+fontSize: 24,
+fontWeight: 'bold',
+color: '#ffffff',
+marginBottom: 8,
+},
+phrase: {
+fontSize: 16,
+fontStyle: 'italic',
+color: '#cccccc',
+textAlign: 'center',
+},
 });
